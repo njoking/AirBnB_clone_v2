0b19c6bf51133ef08849a4b6efe9db5657a56eae
anaco <ndedaanaco@gmail.com>
2024-04-13 00:55:10 +0300

    changed db

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,12 +1,8 @@
--- My SQL setup file for development stage of project
--- Requirements are:
--- creates a database called hbtn_test_db
--- creates a user called hbnb_test in localhost
--- sets the password of the hbnb_test user to hbnb_test_pwd
--- grants the user ALL privileges on the database hbnb_test_db and only this database
--- grants the user SELECT privileges on the database preformance_schema and only this database
--- if the database hbnb_test_db or user hbnb_test already exist the script should not fail
+-- prepares a MySQL server for running tests on the project
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
